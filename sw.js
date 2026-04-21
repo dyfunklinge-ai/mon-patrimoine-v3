09cba03ebbe902362f6ab0cfbd8865a5da353a60
dyfunklinge-ai <dyfunklinge@gmail.com>
2026-04-21 03:04:21 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // On laisse le navigateur gérer normalement sans intercepter
+  return;
+});
